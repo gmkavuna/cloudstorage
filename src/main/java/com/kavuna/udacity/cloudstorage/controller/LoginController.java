4d9f1432b55591a43b347866b88072ba551dc310
Claude K <muhirek@gmail.com>
2021-05-24 22:28:48 -0400

    Making progress in savaging this application. Using an old copy to put reconstruct the application.

--- src/main/java/com/kavuna/udacity/cloudstorage/controller/LoginController.java
+++ src/main/java/com/kavuna/udacity/cloudstorage/controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.kavuna.udacity.cloudstorage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+    @RequestMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
 }
