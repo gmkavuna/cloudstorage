b22553b935397e6a958c4e71fa7009607f71b87c
Claude K <muhirek@gmail.com>
2021-05-24 22:28:48 -0400

    Making progress in salvaging this application. Using an old copy to put reconstruct the application.

---
+++ src/main/java/com/kavuna/udacity/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.kavuna.udacity.cloudstorage.config;
+
+import com.kavuna.udacity.cloudstorage.service.AuthenticationService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider((AuthenticationProvider) this.authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/signup", "/js/**", "/css/**","/static/css/**", "/static/js/**", "/h2/**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
+    }
+}
+
+
+
